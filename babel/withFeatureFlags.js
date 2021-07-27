9f4f8fb8632cd738971fba990465733ee18fc23a
Christoph Kruppe <c.kruppe@nfq.de>
2021-07-27 15:24:29 +0200

    fix(Babel): Remove need for default dev features file

--- babel/withFeatureFlags.js
+++ babel/withFeatureFlags.js
@@ -9,10 +9,6 @@ const path = require('path');
  * @returns {Function} Babel rc function.
  */
 module.exports.withFeatureFlags = (config, options) => api => {
-    if (!process.env.FEATURE_ENV) {
-        process.env.FEATURE_ENV = 'dev';
-    }
-
     if (typeof config === 'function') {
         // eslint-disable-next-line no-param-reassign
         config = config(api);
@@ -25,7 +21,7 @@ module.exports.withFeatureFlags = (config, options) => api => {
         deprecationEnv: options.deprecationEnv ?? 'live',
         featureFlagImport: '@app/features',
         // eslint-disable-next-line security/detect-non-literal-require
-        flags: require(path.resolve(process.cwd(), `./features.${cache}.js`)),
+        flags: cache ? require(path.resolve(process.cwd(), `./features.${cache}.js`)) : {},
         jsxImport: options.jsxImport ?? '@nfq/feature-flags/jsx',
         jsxWithFeature: options.jsxWithFeature ?? 'WithFeature',
         jsxWithoutFeature: options.jsxWithoutFeature ?? 'WithoutFeature'
