770fad56ff77a4386591452b9c931b4a40769d9a
ckruppe <c.kruppe@nfq.de>
2022-05-23 15:08:28 +0200

    feat(ClI): Add type generation (#18)

---
+++ cli.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+
+const generateTypeFiles = require('./generateTypeFiles');
+
+const definition = [
+    {
+        content: 'Generates types for feature flags.',
+        header: 'Feature flag type generator.'
+    },
+    {
+        header: 'Options',
+        optionList: [
+            {
+                alias: 'f',
+                description: 'The import name for feature flags configured in your babel config.',
+                name: 'feature-import',
+                type: String,
+                typeLabel: '{underline String}'
+            },
+            {
+                alias: 'j',
+                description: `The import name for the jsx components that render feature flags. As configured in your
+                babel config.`,
+                name: 'jsx-import',
+                type: String,
+                typeLabel: '{underline String}'
+            },
+            {
+                alias: 'i',
+                description: 'The name of the with feature flag jsx component. As configured in your babel config.',
+                name: 'feature-name',
+                type: String,
+                typeLabel: '{underline String}'
+            },
+            {
+                alias: 'x',
+                description: 'The name of the without feature flag jsx component. As configured in your babel config.',
+                name: 'no-feature-name',
+                type: String,
+                typeLabel: '{underline String}'
+            },
+            {
+                alias: 'e',
+                description: 'The feature env to use the flags from.',
+                name: 'env',
+                type: String,
+                typeLabel: '{underline String}'
+            },
+            {
+                alias: 'h',
+                description: 'Displays this help guide.',
+                name: 'help',
+                type: Boolean
+            }
+        ]
+    }
+];
+
+const help = commandLineUsage(definition);
+
+const options = commandLineArgs(definition[1].optionList);
+
+if (options.help) {
+    process.stdout.write(help);
+} else {
+    generateTypeFiles(
+        options['feature-import'],
+        options['jsx-import'],
+        options['feature-name'],
+        options['no-feature-name']
+    );
+    process.stdout.write('Done');
+}
---
+++ generateTypeFiles.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Generate Type Files.
+ *
+ * @param {String} featureFlagImport The import name for feature flags configured in your babel config.
+ * @param {String} jsxImport         The import name for the jsx components that render feature flags. As configured in your babel config.
+ * @param {String} jsxWithFeature    The name of the with feature flag jsx component. As configured in your babel config.
+ * @param {String} jsxWithoutFeature The name of the without feature flag jsx component. As configured in your babel config.
+ * @param {String} usedEnv           The env to use the flags from.
+ */
+const generateTypeFiles = (featureFlagImport, jsxImport, jsxWithFeature, jsxWithoutFeature, usedEnv) => {
+    const rootPath = process.cwd();
+    const featureJsx = `declare module '${jsxImport}' {
+    interface IFeature {
+        deprecatesOn?: string;
+        feature: string;
+    }
+
+    export class ${jsxWithFeature} extends React.Component<IFeature, any>{};
+    export class ${jsxWithoutFeature} extends React.Component<IFeature, any>{};
+};`;
+
+    try {
+        // eslint-disable-next-line security/detect-non-literal-fs-filename, node/no-sync
+        fs.mkdirSync(`${rootPath}/types`);
+    } catch (e) {}
+
+    // eslint-disable-next-line security/detect-non-literal-fs-filename, node/no-sync
+    fs.writeFileSync(`${rootPath}/types/featureJsx.d.ts`, featureJsx);
+
+    // eslint-disable-next-line security/detect-non-literal-require
+    const flags = require(path.resolve(featureFlagImport, `./features.${usedEnv}.js`)) || {};
+
+    const featureFlags = `declare module '${jsxImport}' {
+    ${Object.keys(flags).map(key => `export const ${key}: bool;`).join('\n')}
+};`;
+
+    // eslint-disable-next-line security/detect-non-literal-fs-filename, node/no-sync
+    fs.writeFileSync(`${rootPath}/types/featureFlags.d.ts`, featureFlags);
+};
+
+module.exports = generateTypeFiles;
