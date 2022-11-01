40ba1cbf163d0fd95a694afef9b97e029fda338c
Christoph Kruppe <c.kruppe@nfq.de>
2022-11-01 10:39:34 +0100

    feat(TranspileModules): Use new next build in transpiling (#27)
    BREAKING CHANGE: Its only working in nextjs 13

--- next/withFeatureFlags.js
+++ next/withFeatureFlags.js
@@ -1,20 +1,33 @@
+/* eslint-disable no-param-reassign */
 const path = require('path');
 
-const withTM = require('next-transpile-modules')(['@nfq/feature-flags']);
-
 module.exports.withFeatureFlags = (nextConfig, {phase}) => {
-    const transpiledConfig = withTM(nextConfig);
+    if (nextConfig.experimental) {
+        nextConfig.experimental = {
+            ...nextConfig.experimental,
+            transpilePackages: ['@nfq/feature-flags']
+        };
+    } else {
+        nextConfig.experimental = {transpilePackages: ['@nfq/feature-flags']};
+    }
 
     return {
-        ...transpiledConfig,
+        ...nextConfig,
+        /**
+         * The webpack config.
+         *
+         * @param {import('webpack').Configuration} config  Webpack config.
+         * @param {object}                          options The webpack options.
+         * @returns {import('webpack').Configuration} Webpack config.
+         */
         webpack(config, options) {
             // eslint-disable-next-line security/detect-non-literal-require, no-param-reassign
             config.cache.version += `|${JSON.stringify(require(path.resolve(process.cwd(), `./features.${process.env.FEATURE_ENV}.js`)))}`;
             // eslint-disable-next-line no-param-reassign
             config.cache.name = `${config.name}-${process.env.FEATURE_ENV}-${phase}`;
 
-            if (typeof transpiledConfig.webpack === 'function') {
-                return transpiledConfig.webpack(config, options);
+            if (typeof nextConfig.webpack === 'function') {
+                return nextConfig.webpack(config, options);
             }
 
             return config;
